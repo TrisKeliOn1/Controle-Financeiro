577e4645f08a9178d7a24e5cf0e19ac617310107
Hebert <hebertLandim007@outlook.com>
2023-01-13 23:43:31 -0300

    Styles Grid

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import GlobalStyle from "./styles/global";
 
 const App = () => {
     const data = localStorage.getItem("transactions");
-    const [transactionsList, setTransactionList] = useState(
-        data ? JSON.parse(data) : []
+    const [transactionsList, setTransactionsList] = useState(
+      data ? JSON.parse(data) : []
     );
     const [income, setIncome] = useState(0);
     const [expense, setExpense] = useState(0);
@@ -15,28 +15,36 @@ const App = () => {
 
     useEffect(() => {
         const amountExpense = transactionsList
-            .filter((item) => item.expense)
-            .map((transaction) => Number(transaction.amount));
-
+          .filter((item) => item.expense)
+          .map((transaction) => Number(transaction.amount));
+    
         const amountIncome = transactionsList
-            .filter((item) => !item.expense)
-            .map((transaction) => Number(transaction.amount));
-
+          .filter((item) => !item.expense)
+          .map((transaction) => Number(transaction.amount));
+    
         const expense = amountExpense.reduce((acc, cur) => acc + cur, 0).toFixed(2);
         const income = amountIncome.reduce((acc, cur) => acc + cur, 0).toFixed(2);
-
+    
         const total = Math.abs(income - expense).toFixed(2);
-
+    
         setIncome(`R$ ${income}`);
         setExpense(`R$ ${expense}`);
         setTotal(`${Number(income) < Number(expense) ? "-" : ""}R$ ${total}`);
-    }, [transactionsList]);
+      }, [transactionsList]);
+
+      const handleAdd = (transaction) => {
+        const newArrayTransactions = [...transactionsList, transaction];
+    
+        setTransactionsList(newArrayTransactions);
+    
+        localStorage.setItem("transactions", JSON.stringify(newArrayTransactions));
+      };
 
     return (
         <>
             <Header />
             <Resume income={income} expense={expense} total={total}/>
-            <Form />
+            <Form handleAdd={handleAdd}/>
             <GlobalStyle />
         </>
     );
