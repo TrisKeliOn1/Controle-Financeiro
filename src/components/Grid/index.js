da28c6cd412ebf507f2ae23cb8a6ccf30cec1c6a
Hebert <hebertLandim007@outlook.com>
2023-01-16 19:55:58 -0300

    Finished

--- src/components/Grid/index.js
+++ src/components/Grid/index.js
@@ -1,27 +1,33 @@
-import React from 'react';
+import React from "react";
 import GridItem from "../GridItem";
 import * as C from "./styles";
 
 const Grid = ({ itens, setItens }) => {
-  
+  const onDelete = (ID) => {
+    const newArray = itens.filter((transaction) => transaction.id !== ID);
+    setItens(newArray);
+    localStorage.setItem("transactions", JSON.stringify(newArray));
+  };
+
   return (
     <C.Table>
       <C.Thead>
         <C.Tr>
-          <C.th width={40}>Descrição</C.th>
-          <C.th width={40}>Valor</C.th>
-          <C.th width={10} alignCenter>Tipo</C.th>
-          <C.th width={10}></C.th>
-
+          <C.Th width={40}>Descrição</C.Th>
+          <C.Th width={40}>Valor</C.Th>
+          <C.Th width={10} alignCenter>
+            Tipo
+          </C.Th>
+          <C.Th width={10}></C.Th>
         </C.Tr>
       </C.Thead>
       <C.Tbody>
         {itens?.map((item, index) => (
-          <GridItem hey={index} item={item} />
+          <GridItem key={index} item={item} onDelete={onDelete} />
         ))}
       </C.Tbody>
     </C.Table>
   );
 };
 
-export default Grid
+export default Grid;
